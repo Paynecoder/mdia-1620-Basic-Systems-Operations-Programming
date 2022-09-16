5dc1afda8655c88071bd1cbcb7d1ccd0bb569035
Joshua Payne <jpayne@Joshuas-MacBook-Air.local>
2022-09-16 15:12:45 -0700

    some-work

--- Part-1/Week-2/test.js
+++ Part-1/Week-2/test.js
@@ -7,3 +7,26 @@ var ElectronicDevices;
 
 // snake case
 var Electronic_Devices;
+
+var age = 14;
+
+console.log(age)
+
+// string
+var name = "Josh"
+
+
+// Boolean
+var proceed = true;
+var i = 0;
+
+while(proceed){
+    i++
+    proceed = false;
+}
+
+console.log(i);
+
+
+
+
